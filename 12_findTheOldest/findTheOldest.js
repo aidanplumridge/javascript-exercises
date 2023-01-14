ebb581b1793dfb2633a60d350d8326e412d6356d
aidanplumridge <92349136+aidanplumridge@users.noreply.github.com>
2023-01-14 00:50:37 +0000

    Solutions through to Fundamentals part 5 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+function findTheOldest(array = []) {
+  return array.reduce((oldest, next) => {
+    const oldestAge = getAge(oldest);
+    const nextAge = getAge(next)
+    return oldestAge < nextAge ? oldest = next : oldest
+  });
+}
 
-};
+
+function getAge (person) {
+  return !person.yearOfDeath ? (new Date()).getFullYear() - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth  
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
